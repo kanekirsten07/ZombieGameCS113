fe7b05c48b1475d1c4b6ff4f9a6e92f553b6b4f5
Kirsten Kane <kanekirsten07@gmail.com>
2014-02-11 15:02:56 -0800

    Projectile system working!

---
+++ ZombieGame/Assets/constantmovement.js
@@ -0,0 +1,7 @@
+﻿#pragma strict
+
+var force : float = 20.0f;
+ 
+function Start() {
+    rigidbody2D.AddForce(new Vector3(700,400,0)); 
+}
---
+++ ZombieGame/Assets/vomitArc.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class vomitArc : MonoBehaviour {
+
+	public float speed = 1.0f;
+	
+	void Update ( )
+	{
+		transform.Translate( Vector3.forward * speed, Space.Self );
+	}
+}

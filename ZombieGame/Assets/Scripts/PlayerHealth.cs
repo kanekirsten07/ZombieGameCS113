675e015f70e8d2b2272bb848cdb3f12fdacb9af7
Kirsten Kane <kanekirsten07@gmail.com>
2014-02-13 16:52:33 -0800

    Connecting the player health bar to each zombie tag

--- ZombieGame/Assets/Scripts/PlayerHealth.cs
+++ ZombieGame/Assets/Scripts/PlayerHealth.cs
@@ -7,19 +7,20 @@ public class PlayerHealth : MonoBehaviour
 	public float repeatDamagePeriod = 2f;		// How frequently the player can be damaged.
 	public AudioClip[] ouchClips;				// Array of clips to play when the player is damaged.
 	public float hurtForce = 10f;				// The force with which the player is pushed when hurt.
-	public float damageAmount = 10f;			// The amount of damage to take when enemies touch the player
+	public float damageAmount ;			// The amount of damage to take when enemies touch the player
 
 	private SpriteRenderer healthBar;			// Reference to the sprite renderer of the health bar.
 	private float lastHitTime;					// The time at which the player was last hit.
 	private Vector3 healthScale;				// The local scale of the health bar initially (with full health).
-	private PlayerControl playerControl;		// Reference to the PlayerControl script.
+	private PlayerControl  playerControl;		// Reference to the PlayerControl script.
+	private ZombiniController ZombiniControl;
 	private Animator anim;						// Reference to the Animator on the player
 
 
 	void Awake ()
 	{
 		// Setting up references.
-		playerControl = GetComponent<PlayerControl>();
+		ZombiniControl = GetComponent<ZombiniController>();
 		healthBar = GameObject.Find("HealthBar").GetComponent<SpriteRenderer>();
 		anim = GetComponent<Animator>();
 
@@ -31,53 +32,73 @@ public class PlayerHealth : MonoBehaviour
 	void OnCollisionEnter2D (Collision2D col)
 	{
 		// If the colliding gameobject is an Enemy...
-		if(col.gameObject.tag == "Enemy")
+		if(col.gameObject.tag == "Walker")
 		{
-			// ... and if the time exceeds the time of the last hit plus the time between hits...
-			if (Time.time > lastHitTime + repeatDamagePeriod) 
+
+			damageAmount = 5f;
+			Debug.Log("Boop");
+		}else if(col.gameObject.tag == "Nurse")
+		{
+			damageAmount = 5f;
+		}else if(col.gameObject.tag == "Cop")
+		{
+			damageAmount = 5f;
+		}else if(col.gameObject.tag == "Flood")
+		{
+			damageAmount = 5f;
+		}else if(col.gameObject.tag == "Minion")
+		{
+			damageAmount = 1f;
+		}
+			handleCollisionStuffs(col);
+		
+	}
+
+
+	void handleCollisionStuffs(Collision2D col)
+	{
+		if (Time.time > lastHitTime + repeatDamagePeriod) 
+		{
+			// ... and if the player still has health...
+			if(health > 0f)
 			{
-				// ... and if the player still has health...
-				if(health > 0f)
+				// ... take damage and reset the lastHitTime.
+				TakeDamage(col.transform); 
+				lastHitTime = Time.time; 
+			}
+			// If the player doesn't have health, do some stuff, let him fall into the river to reload the level.
+			else
+			{
+				// Find all of the colliders on the gameobject and set them all to be triggers.
+				Collider2D[] cols = GetComponents<Collider2D>();
+				foreach(Collider2D c in cols)
 				{
-					// ... take damage and reset the lastHitTime.
-					TakeDamage(col.transform); 
-					lastHitTime = Time.time; 
+					c.isTrigger = true;
 				}
-				// If the player doesn't have health, do some stuff, let him fall into the river to reload the level.
-				else
+				
+				// Move all sprite parts of the player to the front
+				SpriteRenderer[] spr = GetComponentsInChildren<SpriteRenderer>();
+				foreach(SpriteRenderer s in spr)
 				{
-					// Find all of the colliders on the gameobject and set them all to be triggers.
-					Collider2D[] cols = GetComponents<Collider2D>();
-					foreach(Collider2D c in cols)
-					{
-						c.isTrigger = true;
-					}
-
-					// Move all sprite parts of the player to the front
-					SpriteRenderer[] spr = GetComponentsInChildren<SpriteRenderer>();
-					foreach(SpriteRenderer s in spr)
-					{
-						s.sortingLayerName = "UI";
-					}
-
-					// ... disable user Player Control script
-					GetComponent<PlayerControl>().enabled = false;
-
-					// ... disable the Gun script to stop a dead guy shooting a nonexistant bazooka
-					GetComponentInChildren<Gun>().enabled = false;
-
-					// ... Trigger the 'Die' animation state
-					anim.SetTrigger("Die");
+					s.sortingLayerName = "UI";
 				}
+				
+				// ... disable user Player Control script
+				GetComponent<PlayerControl>().enabled = false;
+				
+				// ... disable the Gun script to stop a dead guy shooting a nonexistant bazooka
+				GetComponentInChildren<Gun>().enabled = false;
+				
+				// ... Trigger the 'Die' animation state
+				anim.SetTrigger("Die");
 			}
 		}
 	}
 
-
 	void TakeDamage (Transform enemy)
 	{
 		// Make sure the player can't jump.
-		playerControl.jump = false;
+		ZombiniControl.jump = false;
 
 		// Create a vector that's from the enemy to the player with an upwards boost.
 		Vector3 hurtVector = transform.position - enemy.position + Vector3.up * 5f;
@@ -87,13 +108,14 @@ public class PlayerHealth : MonoBehaviour
 
 		// Reduce the player's health by 10.
 		health -= damageAmount;
+		Debug.Log(health);
 
 		// Update what the health bar looks like.
 		UpdateHealthBar();
 
 		// Play a random clip of the player getting hurt.
-		int i = Random.Range (0, ouchClips.Length);
-		AudioSource.PlayClipAtPoint(ouchClips[i], transform.position);
+		//int i = Random.Range (0, ouchClips.Length);
+		//AudioSource.PlayClipAtPoint(ouchClips[i], transform.position);
 	}
 
 

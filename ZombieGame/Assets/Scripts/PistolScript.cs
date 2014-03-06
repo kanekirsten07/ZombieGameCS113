ccd8916d7b1b27d684d49a3f80b5cd9bcb34909e
Joey Shepard <joey_shepard@sbcglobal.net>
2014-03-06 15:56:00 -0800

    Pistol Script added.

---
+++ ZombieGame/Assets/Scripts/PistolScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PistolScript : MonoBehaviour {
+
+	public GameInfoScript gis;
+
+	// Use this for initialization
+	void Start () {
+		gis = (GameInfoScript)GameObject.Find("GameWorld").GetComponent<GameInfoScript>() as GameInfoScript;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll)
+	{	
+		if (coll.gameObject.tag == "Player")
+		{
+			gis.playerInventoryItem = (GameObject)Instantiate(Resources.Load("Icons/IconPistol"), gis.inventorySlotLocation, transform.rotation);
+			
+			GameObject.Destroy(this.gameObject);
+		}
+	}
+}
